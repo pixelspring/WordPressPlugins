23330123809a28c343d971b63e4b17979386ba35
Mike Seaby <mike@pixelspring.co.uk>
2016-05-17 16:17:23 +0100

    Initial Commit

---
+++ CleanSearch/cleansearch.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: CleanSearch
+Version: 1.0.0
+Plugin URI:
+Description: Redirect www.mysite.com/?s=querystring searches to www.mysite.com/search/querystring
+Author: Mike Seaby
+Author URI: https://pixelspring.co.uk
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+*/
+
+
+/**
+ * I'm sorry Dave, I'm afraid I can't do that
+ */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+
+/**
+ * Redirect URL if search & not blank
+ */
+function pswp_cleansearch() {
+    if ( is_search() && ! empty( $_GET['s'] ) ) {
+        wp_redirect( home_url( "/searching/" ) . urlencode( get_query_var( 's' ) ) );
+        exit();
+    }
+}
+
+/**
+ * Execute the plugin
+ */
+add_action( 'template_redirect', 'pswp_cleansearch' );
---
+++ README.md
@@ -0,0 +1,7 @@
+## WordPress Plugins
+
+A work in progress collection of plugins for WordPress to implement various features
+
+## License
+
+MIT
